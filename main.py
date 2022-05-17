1a699553be55adcb22931667951182adb08b2ec5
BeMain <bemain.dev@gmail.com>
2022-05-17 09:54:58 +0200

    Install pygmsh for generating mesh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import pygmsh
 from lsystem_renderer import LSystemRenderer
 from lsystem import *
 import yaml
+
 
 lsys = LSystem("F", {
     "F": "Y[++++++MF][-----NF][^^^^^OF][&&&&&PF]",
@@ -22,3 +24,18 @@ with open("fagus_sylvatica.yaml", "r") as file:
 
 renderer = LSystemRenderer.from_yaml(data["rendering"]["stem"])
 print(renderer)
+
+
+with pygmsh.occ.Geometry() as geom:
+    geom.characteristic_length_max = 0.1
+    ellipsoid = geom.add_ellipsoid([0.0, 0.0, 0.0], [1.0, 0.7, 0.5])
+
+    cylinders = [
+        geom.add_cylinder([-1.0, 0.0, 0.0], [2.0, 0.0, 0.0], 0.3),
+        geom.add_cylinder([0.0, -1.0, 0.0], [0.0, 2.0, 0.0], 0.3),
+    ]
+    geom.boolean_difference(ellipsoid, geom.boolean_union(cylinders))
+
+    mesh = geom.generate_mesh()
+
+    pygmsh.helpers.gmsh.fltk.run()
