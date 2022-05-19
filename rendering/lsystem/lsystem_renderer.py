03e428908b27ce3e6a8a60a3d2f06f4d8c2d0490
BeMain <bemain.dev@gmail.com>
2022-05-19 16:37:22 +0200

    Automatically decrease Segment diameter
    diameter_ratio can be set to control how much diameter should be decreased
    from start to end of Segment

--- rendering/lsystem/lsystem_renderer.py
+++ rendering/lsystem/lsystem_renderer.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from math import radians
 import pygmsh
+import numpy as np
 import numpy.typing as npt
 
 from rendering.lsystem.transform import Transform
@@ -10,17 +11,24 @@ from .lsystem import LSystem
 from .segment import Segment
 
 
+class RenderState(Transform):
+    def __init__(self, position: npt.NDArray = np.array([0.0, 0.0, 0.0]), rotation: npt.NDArray = np.array([0.0, 0.0, 0.0]), diameter: float = 0.5) -> None:
+        super().__init__(position, rotation)
+
+        self.diameter = diameter
+
+
 class LSystemRenderer:
     """Uses a LSystem to generate instructions, and then renders a mesh based on those instructions."""
 
-    def __init__(self, type: str = "LSystem", branching_angle: float = None, phyllotactic_angle: float = None, controls: dict[str, str] = None, axiom: str = None, rules: dict[str, str] = None) -> None:
+    def __init__(self, type: str = "LSystem", branching_angle: float = None, diameter_ratio: float = None, controls: dict[str, str] = None, axiom: str = None, rules: dict[str, str] = None) -> None:
         self.branching_angle = branching_angle
-        self.phyllotactic_angle = phyllotactic_angle
+        self.diameter_ratio = diameter_ratio
         self.controls = {value: key for key, value in controls.items()}
         self.rotation_matrices = self.get_rotation_matrices(controls)
         self.lsystem = LSystem(axiom, rules)
 
-        self.state = Transform()
+        self.state = RenderState()
 
     def get_rotation_matrices(self, controls) -> dict[str, npt.NDArray]:
         angle = radians(10)
@@ -40,7 +48,7 @@ class LSystemRenderer:
         segments = []
 
         segment_length: int = 0
-        states: list[Transform] = []
+        states: list[RenderState] = []
 
         for instruction in self.lsystem.axiom:
             if not instruction in self.controls:  # Increase length of next segment
@@ -48,10 +56,18 @@ class LSystemRenderer:
 
             else:
                 if segment_length >= 1:  # Create new segment
-                    segments.append(
-                        deepcopy(Segment(segment_length, self.state.position, self.state.rotation)))
+                    diameter_mod = pow(self.diameter_ratio, segment_length)
+                    segments.append(deepcopy(
+                        Segment(segment_length,
+                                self.state.diameter,
+                                self.state.diameter * diameter_mod,
+                                self.state.position,
+                                self.state.rotation,
+                                )
+                    ))
                     self.state.position += self.state.rotation_matrix() @ \
                         [segment_length, 0, 0]
+                    self.state.diameter *= diameter_mod
 
                     segment_length = 0
 
@@ -71,8 +87,11 @@ class LSystemRenderer:
         with pygmsh.occ.Geometry() as geom:
             #geom.characteristic_length_max = 0.1
             for segment in segments:
-                cyl = geom.add_cylinder(
-                    segment.position, segment.rotation_matrix() @ [segment.length, 0, 0], 0.3)
+                cyl = geom.add_cone(
+                    segment.position,
+                    segment.rotation_matrix() @ [segment.length, 0, 0],
+                    segment.top_d,
+                    segment.bottom_d)
 
             mesh = geom.generate_mesh()
             mesh.write("test.vtk")
--- rendering/lsystem/segment.py
+++ rendering/lsystem/segment.py
@@ -6,10 +6,12 @@ from rendering.lsystem.transform import Transform
 
 
 class Segment(Transform):
-    def __init__(self, length: float, position: npt.NDArray = np.array([0.0, 0.0, 0.0]), rotation: npt.NDArray = np.array([0.0, 0.0, 0.0])) -> None:
+    def __init__(self, length: float, top_diameter: float, bottom_diameter: float, position: npt.NDArray = np.array([0.0, 0.0, 0.0]), rotation: npt.NDArray = np.array([0.0, 0.0, 0.0])) -> None:
+        super().__init__(position, rotation)
+
         self.length = length
-        self.position = position
-        self.rotation = rotation
+        self.top_d = top_diameter
+        self.bottom_d = bottom_diameter
 
     def __repr__(self):
         return f"Segment({self.length}; {self.position}, {self.rotation})"
